dce78ac7019625113d6833d184ab23e9566a3f0d
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-05-16 12:42:27 +0600

    fix bugs spaces in table model

--- app.py
+++ app.py
@@ -19,10 +19,12 @@ class Movie(db.Model):
     trailer = db.Column(db.String(255))
     year = db.Column(db.Integer)
     rating = db.Column(db.Float)
+
     genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    genre = db.relationship("Genre")
+    genre = db.relationship("genre")
+
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-    director = db.relationship("Director")
+    director = db.relationship("director")
 
 
 class Director(db.Model):
